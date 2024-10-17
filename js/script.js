c617be8a1f060b8724c2a7099e5571d2c2cc280a
Arthur Leandro Pereira <145617892+ArthurLpereira@users.noreply.github.com>
2024-10-17 15:44:42 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,64 @@
-const dias = document.getElementsByClassName('dia');
-const data = document.getElementById('data');
-for (let dia of dias) {
-    dia.addEventListener('click', function () {
-        for (let i of dias){
-            i.classList.remove('selecionado');
+const atual = document.getElementById('atual');
+const dias = document.getElementById('dias');
+const btnVoltar = document.getElementById('anterior');
+const btnProximo = document.getElementById('proximo')
+let  MesAtual = 9;
+let ano = 2024;
+
+const meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho","Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+function GerarDias(mes, ano){
+    dias.innerHTML = "";
+    const DiaSTotal = new Date (ano, mes + 1, 0).getDate();
+    const primeiroDiaSemana = new Date (ano, mes, 1).getDay();
+
+    for (let i = 0; i <primeiroDiaSemana; i++){
+        const outro_mes = document.createElement('div');
+        outro_mes.classList.add('vazio')
+        dias.appendChild(outro_mes);
+    }
+
+    
+    for(let dia = 1; dia <=DiaSTotal; dia++){
+        const diaElemento = document.createElement('div');
+        diaElemento.classList.add('dia');
+        diaElemento.textContent = dia;
+
+        const final_semana = new Date(ano,mes,dia).getDay()
+        if (final_semana === 0 || final_semana === 6){
+            diaElemento.classList.add('fim_semana');
         }
-        dia.classList.add('selecionado');
 
-        const mostrar_dia_escolhido = dia.querySelector('p').textContent;
-        const mes = 'outubro'
-        const ano = '2024'
+        diaElemento.addEventListener('click',  () =>{
+            document.querySelectorAll('.dia').forEach(i => i.classList.remove('selecionado'));
+            diaElemento.classList.add('selecionado');
+        });
 
-        data.textContent = `Dia ${mostrar_dia_escolhido} de ${mes} de ${ano}`;
-    });
+        dias.appendChild(diaElemento);
+    }
 }
+
+function CarregarCalendario(){
+    atual.textContent = `${meses[MesAtual]} ${ano}`
+    GerarDias(MesAtual, ano);
+}
+
+btnVoltar.addEventListener('click', () =>{
+    MesAtual--;
+    if(MesAtual < 0){
+        MesAtual = 11;
+        ano --
+    }
+    CarregarCalendario()
+})
+
+btnProximo.addEventListener('click', () =>{
+    MesAtual++
+    if(MesAtual > 11){
+        MesAtual = 0
+        ano++
+    }
+    CarregarCalendario()
+})
+
+CarregarCalendario()
